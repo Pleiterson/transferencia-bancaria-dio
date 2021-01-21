0750dc27c3beccc928d0674a7f2d97f5d2aecac7
pleiterson <pleiterson@gmail.com>
2021-01-21 14:08:37 -0300

    create project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace transferencia_bancaria_dio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
